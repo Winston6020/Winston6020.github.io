d6a69622ac665979b2be335f53ba5e772a368de5
George1602181 <1602181@spsd.sk.ca>
2024-09-20 11:02:54 -0600

    Updated Interactive scene

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,10 +6,30 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
+x = 0
+y = 0
+let speed = 5
+let squareSize = 50
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+  square(x, y, squareSize)
+  moveSquare()
+}
+
+function moveSquare(){
+  if (mouseIsPressed) {
+    if (mouseButton === LEFT) {
+      x += speed;
+      y += speed;
+    }
+    if(mouseButton === RIGHT) {
+      x -= speed;
+      y -= speed;
+    }
+  }
 }
