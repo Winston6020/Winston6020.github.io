6d869e8f13ddd5bb6b8208ccb1610051eba87090
George1602181 <1602181@spsd.sk.ca>
2024-09-26 10:24:59 -0600

    interactive scene

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -11,33 +11,38 @@ let x = 0;
 let y = 0;
 let speed = 5;
 let squareSize = 50;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  square(x, y, squareSize);
+  //square(x, y, squareSize);
   moveSquare();
 }
 
 function moveSquare() {
   if (mouseIsPressed) {
     if (mouseButton === LEFT) {
-      fill ("black");
+      //fill ("black");
       square (mouseX, mouseY, squareSize);
     }
   }
   if(keyIsDown(UP_ARROW)) {
+    square (x, y, squareSize);
     y -= speed;
   }
   if(keyIsDown(DOWN_ARROW)) {
+    square (x, y, squareSize);
     y += speed;
   }
   if(keyIsDown(LEFT_ARROW)) {
+    square (x, y, squareSize);
     x -= speed;
   }
   if(keyIsDown(RIGHT_ARROW)) {
+    square (x, y, squareSize);
     x += speed;
   }
 }
