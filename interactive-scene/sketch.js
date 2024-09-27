a1ec0fe9a3a844a8f237c038b00822710928a7b5
George1602181 <1602181@spsd.sk.ca>
2024-09-27 11:02:09 -0600

    now can resize the square with mouse wheel

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,26 +6,30 @@
 // - describe what you did to take this project "above and beyond"
 
 
-
+//Defining the Varibles
 let x = 0;
 let y = 0;
 let speed = 5;
 let squareSize = 50;
+let direction = 0;
 
+//Creating the Canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+//Drawing the Square
 function draw() {
   background(220);
-  //square(x, y, squareSize);
+  square(x, y, squareSize);
   moveSquare();
 }
 
+//Funtion that allows us to move the square with the mouse 
+//and arrow keys
 function moveSquare() {
   if (mouseIsPressed) {
     if (mouseButton === LEFT) {
-      //fill ("black");
       square (mouseX, mouseY, squareSize);
     }
   }
@@ -44,5 +48,17 @@ function moveSquare() {
   if(keyIsDown(RIGHT_ARROW)) {
     square (x, y, squareSize);
     x += speed;
+  }
+}
+
+//Allows us to adjust the size of the square by scrolling
+function mouseWheel(event) {
+  if (event.delta > 0) {
+    direction = "▲";
+    squareSize += 10;
+  } 
+  else {
+    direction = "▼";
+    squareSize -= 10;
   }
 }
