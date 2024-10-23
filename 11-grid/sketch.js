fd0c472fff911f76ca343939637c10d391be7aac
George1602181 <1602181@spsd.sk.ca>
2024-10-23 10:59:15 -0600

    updated grid

--- 11-grid/sketch.js
+++ 11-grid/sketch.js
@@ -28,6 +28,17 @@ function setup() {
   grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
 }
 
+function windowResized() {
+  if (windowWidth < windowHeight) {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+  else {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+
+  cellSize = height/GRID_SIZE;
+}
+
 function draw() {
   background(220);
   displayGrid();
@@ -42,17 +53,31 @@ function keyPressed() {
   }
 }
 
-function mouseIsPressed(mouseX, mouseY){
-  for (let y = 0; y < GRID_SIZE; y++){
-    for (let x = 0; x < GRID_SIZE; x++) {
-      if (mouseX < GRID_SIZE && mouseY < GRID_SIZE && grid[y][x] === 1) {
-        fill ("white");
-      }
-      else if (mouseX < GRID_SIZE && mouseY < GRID_SIZE && grid[y][x] === 0) {
-        fill ("black");
-      }
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  //toggle self
+  toggleCell(x, y);
+
+  //toggle neighbors
+  toggleCell(x-1, y);
+  toggleCell(x+1, y);
+  toggleCell(x, y-1);
+  toggleCell(x, y+1);
+}
+
+function toggleCell(x, y) {
+  //make sure the cell youre toggling is in the grid
+  if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE){
+    if(grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+    else {
+      grid[y][x] = 0;
     }
   }
+  
 }
 
 function displayGrid() {
