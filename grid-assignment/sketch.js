ab826ec40fe185dd71eebc16940b05bec1341909
George1602181 <1602181@spsd.sk.ca>
2024-10-29 11:00:07 -0600

    trying to create a checkerboard

--- grid-assignment/sketch.js
+++ grid-assignment/sketch.js
@@ -6,7 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 const CELL_SIZE = 80;
-let grid;
+let grid =[[1, 0, 1, 0, 1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],[0, 1, 0, 1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],[0, 1, 0, 1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],[0, 1, 0, 1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]];
 let cols;
 let rows;
 
@@ -24,32 +24,37 @@ function draw() {
   displayGrid();
 }
 
-function displayGrid() {
+function displayGrid(x, y) {
+  let newGrid = [];
   for(let y = 0; y < cols; y ++) {
     for(let x = 0; x < rows; x ++) {
       if (grid[x][y] === 1) {
+        newGrid[y].push(0);
         fill ("black");
       }
       else if (grid[x][y] === 0) {
+        newGrid[y].push(1);
         fill("white");
+        
       }
+      return newGrid;
       square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
     }
   }
 }
 
-function generateRandomGrid() {
-  newGrid = [];
-  for(let y = 0; y < rows; y++) {
-    newGrid.push([]);
-    for (let x = 0; x < cols; x ++) {
-      if (random(1) < 1) {
-        newGrid[y].push(0);
-      }
-      else {
-        newGrid[y].push(1);
-      }
-    }
-  }
-  return newGrid;
-}
+// function generateRandomGrid() {
+//   newGrid = [];
+//   for(let y = 0; y < rows; y++) {
+//     newGrid.push([]);
+//     for (let x = 0; x < cols; x ++) {
+//       if (random(1) < 1) {
+//         newGrid[y].push(0);
+//       }
+//       else {
+//         newGrid[y].push(1);
+//       }
+//     }
+//   }
+//   return newGrid;
+// }
