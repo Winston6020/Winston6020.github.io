2d8da41477db727fde29f3b3aa630fea0fe239dd
George1602181 <1602181@spsd.sk.ca>
2024-10-30 11:02:04 -0600

    tried figuring out how to create a checker board

--- grid-assignment/sketch.js
+++ grid-assignment/sketch.js
@@ -5,8 +5,8 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const CELL_SIZE = 80;
-let grid =[[1, 0, 1, 0, 1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],[0, 1, 0, 1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],[0, 1, 0, 1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],[0, 1, 0, 1, 0, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]];
+const CELL_SIZE = 90;
+let grid;
 let cols;
 let rows;
 
@@ -24,37 +24,35 @@ function draw() {
   displayGrid();
 }
 
-function displayGrid(x, y) {
-  let newGrid = [];
-  for(let y = 0; y < cols; y ++) {
-    for(let x = 0; x < rows; x ++) {
-      if (grid[x][y] === 1) {
-        newGrid[y].push(0);
+function displayGrid() {
+  for(let y = 0; y < rows; y ++) {
+    for(let x = 0; x < cols; x ++) {
+      if (grid[y][x] === 1) {
         fill ("black");
       }
-      else if (grid[x][y] === 0) {
-        newGrid[y].push(1);
+      else if (grid[y][x] === 0) {
         fill("white");
-        
       }
-      return newGrid;
-      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+      square (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
     }
   }
 }
 
-// function generateRandomGrid() {
-//   newGrid = [];
-//   for(let y = 0; y < rows; y++) {
-//     newGrid.push([]);
-//     for (let x = 0; x < cols; x ++) {
-//       if (random(1) < 1) {
-//         newGrid[y].push(0);
-//       }
-//       else {
-//         newGrid[y].push(1);
-//       }
-//     }
-//   }
-//   return newGrid;
-// }
+function generateRandomGrid(cols, rows) {
+  let shouldPushOne = true;
+  newGrid = [];
+  for(let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for(let x = 0; x < cols; x ++) {
+      if (shouldPushOne) {
+        newGrid[y].push(1);
+      }
+      else {
+        newGrid[y].push(0);
+      }
+      shouldPushOne = !shouldPushOne;      
+    }
+    shouldPushOne = !shouldPushOne; 
+  }
+  return newGrid;
+}
