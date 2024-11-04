5b97f5b20f8040cb3085df31f133f076fb75a57b
George1602181 <1602181@spsd.sk.ca>
2024-11-04 11:02:02 -0600

    got the checkers lined up properly now need to figure out how to move them

--- grid-assignment/sketch.js
+++ grid-assignment/sketch.js
@@ -14,8 +14,8 @@ let rows;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cols = Math.floor(width/CELL_SIZE);
-  rows = Math.floor(height/CELL_SIZE);
+  cols = 8;
+  rows = 8;
   grid = generateRandomGrid(cols, rows);
 }
 
@@ -23,6 +23,8 @@ function draw() {
   background(220);
   displayGrid();
   spawnCheckers();
+  // generateCheckers();
+  // displayCheckers();
 }
 
 function displayGrid() {
@@ -61,10 +63,48 @@ function generateRandomGrid(cols, rows) {
 function spawnCheckers () {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      if ((y < 3 || y > 4) && grid[y][x] === 1) {
-        circle(x * CELL_SIZE + 45, y * CELL_SIZE + 45, CELL_SIZE);
+      if (y < 3 && grid[y][x] === 1) {
         fill ("red");
+        circle(x * CELL_SIZE + 45, y * CELL_SIZE + 45, CELL_SIZE);
+      }
+      else if (y > 4 && grid [y][x] === 1) {
+        fill ("white");
+        circle(x * CELL_SIZE + 45, y * CELL_SIZE + 45, CELL_SIZE);
       }
     }
   }
 }
+
+function moveCheckers() {
+  
+}
+
+// function generateCheckers(cols, rows) {
+//   let shouldPushOne = true;
+//   newCheckers = [];
+//   for(let y = 0; y < rows; y++) {
+//     newGrid.push([]);
+//     for(let x = 0; x < cols; x ++) {
+//       if (shouldPushOne) {
+//         newGrid[y].push(1);
+//       }
+//       else {
+//         newGrid[y].push(0);
+//       }
+//       shouldPushOne = !shouldPushOne;      
+//     }
+//     shouldPushOne = !shouldPushOne; 
+//   }
+//   return newCheckers;
+// }
+
+// function displayCheckers() {
+//   for(let y = 0; y < 3; y ++) {
+//     for(let x = 0; x > 4; x ++) {
+//       if (grid[y][x] === 1) {
+//         fill ("red");
+//         circle (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+//       }
+//     }
+//   }
+// }
