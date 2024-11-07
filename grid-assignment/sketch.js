27043956b1a6d67330a86999a37e43d11f7951d5
George1602181 <1602181@spsd.sk.ca>
2024-11-07 10:23:48 -0600

    still trying to move the pieces

--- grid-assignment/sketch.js
+++ grid-assignment/sketch.js
@@ -10,10 +10,6 @@ let grid;
 let cols;
 let rows;
 const CHECKERS = 2;
-// let theCheckers = {
-//   x: 0,
-//   y: 0,
-// };
 
 
 
@@ -22,15 +18,15 @@ function setup() {
   cols = 8;
   rows = 8;
   grid = generateRandomGrid(cols, rows);
-  //grid = [theCheckers.y][theCheckers.x] = CHECKERS;
+  grid = [CHECKERS.y][CHECKERS.x] = CHECKERS;
 }
 
 function draw() {
   background(220);
   displayGrid();
-  spawnCheckers();
-  // generateCheckers();
-  // displayCheckers();
+  // spawnCheckers();
+  generateCheckers();
+  displayCheckers();
 }
 
 function displayGrid() {
@@ -66,20 +62,22 @@ function generateRandomGrid(cols, rows) {
   return newGrid;
 }
 
-function spawnCheckers () {
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      if (y < 3 && grid[y][x] === 1) {
-        fill ("red");
-        circle(x * CELL_SIZE + 45, y * CELL_SIZE + 45, CELL_SIZE);
-      }
-      else if (y > 4 && grid [y][x] === 1) {
-        fill ("white");
-        circle(x * CELL_SIZE + 45, y * CELL_SIZE + 45, CELL_SIZE);
-      }
-    }
-  }
-}
+// function spawnCheckers () {
+//   for (let y = 0; y < rows; y++) {
+//     for (let x = 0; x < cols; x++) {
+//       if (y < 3 && grid[y][x] === 1) {
+//         fill ("red");
+//         circle(x * CELL_SIZE + 45, y * CELL_SIZE + 45, CELL_SIZE);
+        
+//       }
+//       else if (y > 4 && grid [y][x] === 1) {
+//         fill ("white");
+//         circle(x * CELL_SIZE + 45, y * CELL_SIZE + 45, CELL_SIZE);
+        
+//       }
+//     }
+//   }
+// }
 
 function mousePressed() {
   let x = Math.floor(mouseX/CELL_SIZE);
@@ -88,40 +86,51 @@ function mousePressed() {
 }
 
 function liftPiece() {
-  if (grid[y][x] === 2) {
-    grid[y][x] === 1;
-  }
-  else {
-    grid[y][x] = grid[y][x];
+  if (grid[y][x] === 1) {
+    square (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    toggleCell(x, y);
   }
 }
 
-// function generateCheckers(cols, rows) {
-//   let shouldPushOne = true;
-//   newCheckers = [];
-//   for(let y = 0; y < rows; y++) {
-//     newGrid.push([]);
-//     for(let x = 0; x < cols; x ++) {
-//       if (shouldPushOne) {
-//         newGrid[y].push(1);
-//       }
-//       else {
-//         newGrid[y].push(0);
-//       }
-//       shouldPushOne = !shouldPushOne;      
-//     }
-//     shouldPushOne = !shouldPushOne; 
-//   }
-//   return newCheckers;
-// }
+function toggleCell(x, y) {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (y < 3 && grid [y][x] === 1) {
+        fill("black");
+      }
+      if (y > 4 && grid[y][x] === 1) {
+        fill("black");
+      }
+    }
+  }
+}
 
-// function displayCheckers() {
-//   for(let y = 0; y < 3; y ++) {
-//     for(let x = 0; x > 4; x ++) {
-//       if (grid[y][x] === 1) {
-//         fill ("red");
-//         circle (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
-//       }
-//     }
-//   }
-// }
+function generateCheckers(cols, rows) {
+  let shouldPushTwo = true;
+  let CHECKERS = [];
+  for(let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for(let x = 0; x < cols; x ++) {
+      if (shouldPushTwo) {
+        newGrid[y].push(2);
+      }
+      else {
+        newGrid[y].push(0);
+      }
+      shouldPushTwo = !shouldPushTwo;      
+    }
+    shouldPushTwo = !shouldPushTwo; 
+  }
+  return CHECKERS;
+}
+
+function displayCheckers() {
+  for(let y = 0; y < 3; y ++) {
+    for(let x = 0; x > 4; x ++) {
+      if (grid[y][x] === 1) {
+        fill ("red");
+        circle (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+      }
+    }
+  }
+}
