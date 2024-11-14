bdf90c7d823258922fdf24b49074219c170dd175
George1602181 <1602181@spsd.sk.ca>
2024-11-14 10:14:43 -0600

    walker

---
+++ 17-walker-oop-array - Copy/sketch.js
@@ -0,0 +1,63 @@
+// Walker OOP Demo
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+class Walker {
+  constructor(x, y, theColor) {
+    this.x = x;
+    this.y = y;
+    this.speed = 10;
+    this.radius = 5;
+    this.color = theColor;
+  }
+
+  display() {
+    fill(this.color);
+    circle(this.x, this.y, this.radius * 2);
+  }
+  
+  move() {
+    let choice = random(100);
+    if(choice < 25) {
+      //up
+      this.y -= this.speed;
+    }
+    else if(choice < 50) {
+      //down
+      this.y += this.speed;
+    }
+    else if (choice < 75) {
+      //left
+      this.x -= this.speed;
+    }
+    else{
+      //right
+      this.x += this.speed;
+    }
+  }
+}
+
+let walkerArray = [];
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  let winston = new Walker(width/2, height/2, "red");
+  walkerArray.push(winston);
+}
+
+function draw() {
+  for (let theWalker of walkerArray) {
+    theWalker.move();
+    theWalker.display();
+  }
+}
+
+function mousePressed() {
+  let randomColor = color(random(255), random(255), random(255));
+  let someWalker = new Walker (mouseX, mouseY, randomColor);
+  walkerArray.push(someWalker);
+}
