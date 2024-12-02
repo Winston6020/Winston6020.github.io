b9131f7203941c1a9c3d3bc64f7ed1d5cda73f79
George1602181 <1602181@spsd.sk.ca>
2024-12-02 10:16:52 -0600

    local storage

---
+++ 21-local-storage/sketch.js
@@ -0,0 +1,43 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let numberOfClicks = 0;
+let highestClick = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //only get the highest if it exists
+  if (getItem("highest")) {
+    highestClick = getItem("highest");
+  }
+}
+
+function draw() {
+  background(220);
+  displayClicks();
+  displayHighest();
+}
+
+function mousePressed() {
+  numberOfClicks++;
+  if(numberOfClicks > highestClick) {
+    highestClick = numberOfClicks;
+    storeItem("highest", highestClick);
+  }
+}
+
+function displayClicks() {
+  fill("black");
+  textSize(50);
+  text(numberOfClicks, 100, height/2);
+}
+
+function displayHighest() {
+  fill("green");
+  textSize(50);
+  text(highestClick, 400, height/2);
+}
